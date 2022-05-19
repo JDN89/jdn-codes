7d6f964923ce0ac0068a031fa7b30065b5e24888
JDN89 <jan.de.niels@gmail.com>
2022-05-19 16:29:39 -0500

    initial setup

---
+++ src/pages/index.vue
@@ -0,0 +1,13 @@
+<script setup lang="ts">
+let test = "hello"
+console.log(test);
+
+</script>
+
+<template>
+    <div>
+        <h1>Say Hello</h1>
+        <li>create border for termianl use yakuake and solorized colors
+        </li>
+    </div>
+</template>
